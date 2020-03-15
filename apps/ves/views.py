090453fdf79087e1eac50b391024a3efafe4ad7c
svalench <chitsalex@gmial.com>
2020-03-15 11:46:15 -0700

    добавил вывод действий оператора добавил в авто еще действия

--- apps/ves/views.py
+++ apps/ves/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
@@ -66,9 +67,16 @@ class DataView(LoginRequiredMixin, CreateView):
 
     @login_required
     def ActionView(request):
-        action = ActionUser.objects.all()
-        data = {'actions': action}
+        action = ActionUser.objects.all().order_by("-date_add")
+        print(action)
+        paginator = Paginator(action, 10)  # Show 25 contacts per page
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        data = {'page_obj': page_obj}
         return render(request, 'data/action_view.html', data)
+
+
 
     @login_required
     def AgentView(request):
